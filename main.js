af084e43e98268b3af3ef75747798345894a5abb
vasuishq <vasu.227vasavi@gmail.com>
2018-03-29 12:27:36 +0530

    initial commit

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ loadjson("data.json", function (text) {
   let data=JSON.parse(text);
   console.log(data);
   basics(data.left);
+  education(data.education);
+  skill(data.skills)
 })
 // for main div class calling
 // var main=document.queryselector(".main");
@@ -38,10 +40,52 @@ left.appendChild(image);
 var name=document.createElement("h1");
 name.textContent=leftside.name;
 left.appendChild(name);
-var email=document.createElement("h2");
+var email=document.createElement("h3");
 email.textContent=leftside.email;
 name.appendChild(email);
 var ph=document.createElement("p");
 ph.textContent=leftside.phone;
 email.appendChild(ph);
+console.log(left);
+}
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+// education div start
+var edu=document.createElement("h3");
+edu.classList.add("edu1");
+edu.textContent="Education Details:";
+edu.appendChild(document.createElement("HR"));
+right.appendChild(edu);
+function education(educa) {
+for(i in educa){
+var e1=document.createElement("div");
+
+e1.classList.add("edu2")
+e1.textContent=educa[i].course;
+var ul=document.createElement("ul");
+for(j in educa[i].college){
+  var li=document.createElement("li");
+  li.textContent=educa[i].college[j];
+ul.appendChild(li);
+e1.appendChild(ul);
+edu.appendChild(e1);
+}
+}
+  }
+function skill(skills){
+  var skill_title=document.createElement("h4");
+  skill_title.classList.add("tech_skills");
+  skill_title.textContent="technical skills:";
+  skill_title.appendChild(document.createElement("HR"));
+  right.appendChild(skill_title);
+  // table creation
+  var table=document.createElement("table");
+  var row="";
+  for (var i = 0; i < skills.length; i++) {
+  row =row+"<tr><td>"+skills[i].name+"</td><td>"+skills[i].value+"</td></tr>"
+
+  }
+  table.innerHTML=row;
+  skill_title.appendChild(table);
 }
